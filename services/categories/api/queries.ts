f06fcf02afd52a79dc5b07a812b4571de1595e43
simurgh420 <52625819+simurgh420@users.noreply.github.com>
2025-12-28 12:53:07 +0330

    fix(queries): remove type

--- services/categories/api/queries.ts
+++ services/categories/api/queries.ts
@@ -4,10 +4,10 @@ import { Category, CategoryWithProducts } from '@/types/category';
 import axios from 'axios';
 
 export async function fetchCategories(): Promise<Category[]> {
-  const res = await axios.get<Category[]>('/api/categories');
+  const res = await axios.get('/api/categories');
   return res.data;
 }
 export async function fetchCategoryBySlug(slug: string): Promise<CategoryWithProducts> {
-  const res = await axios.get<CategoryWithProducts>(`/api/categories/${slug}`);
+  const res = await axios.get(`/api/categories/${slug}`);
   return res.data;
 }
