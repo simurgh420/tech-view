4b944b2bc9694de93b5d8a02e6f67716d11152c4
simurgh420 <52625819+simurgh420@users.noreply.github.com>
2026-05-17 09:59:54 +0330

    test(comment): :bug: fix type bug and add AdminComment type

--- hooks/useAdmin/useAdminComments.ts
+++ hooks/useAdmin/useAdminComments.ts
@@ -3,11 +3,12 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { fetchAllCommentsAdminApi } from '@/services/comments/api/queries';
 import { deleteCommentApi } from '@/services/comments/api/mutations';
+import { AdminComment } from '@/types/comment';
 
 export function useAdminComments() {
   const qc = useQueryClient();
 
-  const commentsQuery = useQuery({
+  const commentsQuery = useQuery<AdminComment[]>({
     queryKey: ['admin-comments'],
     queryFn: fetchAllCommentsAdminApi,
   });
