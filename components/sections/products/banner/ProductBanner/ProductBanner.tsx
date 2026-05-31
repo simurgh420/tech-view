edfa28942bb364222823327fd60a2c5a1c9addff
simurgh420 <52625819+simurgh420@users.noreply.github.com>
2026-05-31 13:07:21 +0330

    fix(blug): :fire: Banner image warning fix

--- components/sections/products/banner/ProductBanner/ProductBanner.tsx
+++ components/sections/products/banner/ProductBanner/ProductBanner.tsx
@@ -6,13 +6,15 @@ export function ProductBanner() {
       <div className=" h-auto">
         {/* آیفون 17 */}
         <Link href="/products/iphone17">
-          <Image
-            src="/img/banners/iphone17.png"
-            alt="iPhone 17 Banner"
-            width={1224}
-            height={420}
-            className="object-cover w-full h-full"
-          />
+          <div className="relative w-full aspect-1224/420">
+            <Image
+              src="/img/banners/iphone17.png"
+              alt="iPhone 17 Banner"
+              width={1224}
+              height={420}
+              className="object-cover w-full h-full"
+            />
+          </div>
         </Link>
       </div>
     </section>
